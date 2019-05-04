af1c2296a3aa7907f2e3cc193c3184ff69698889
Bob Barrett <bob.barrett@confluent.io>
2019-05-03 18:00:56 -0700

    Delete mk-include

--- mk-include/cc-node.mk
+++
@@ -1,6 +0,0 @@
-.PHONY: set-node-bumped-version
-set-node-bumped-version:
-	test -f package.json \
-		&& (npm version $(BUMPED_VERSION) --git-tag-version=false &&\
-			git add package.json) \
-		|| true
