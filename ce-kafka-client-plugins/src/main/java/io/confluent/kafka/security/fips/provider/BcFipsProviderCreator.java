03d32809ef2a76060950af835afeab06c948be12
Jeff Huang <47870461+jeffhuang26@users.noreply.github.com>
2019-09-23 10:33:45 -0700

    FIxed Bouncy Castle FIPS provider hang issue on RHEL 7 with FIPS enabled. (#753)
    * FIxed Bouncy Castle FIPS provide hang issue on RHEL 7 with FIPS enabled kernel.

--- ce-kafka-client-plugins/src/main/java/io/confluent/kafka/security/fips/provider/BcFipsProviderCreator.java
+++ ce-kafka-client-plugins/src/main/java/io/confluent/kafka/security/fips/provider/BcFipsProviderCreator.java
@@ -18,9 +18,15 @@ public class BcFipsProviderCreator implements SecurityProviderCreator {
     public BcFipsProviderCreator() {}
 
     /**
-     * Create the security provider configured
+     * Create the security provider configured. The constructor parameter is for configuring DRBG.
+     * The following comment is from BC FIPS user guide:
+     * In order to make the default DRBG suitable for key generation, the default DRBG is configured to be prediction
+     * resistant and this can strain the JVMs entropy source especially if hardware RNG is not available.
+     * In situations where the amount of entropy is constrained the default DRBG for the provider can be configured to
+     * use an DRBG chain based on a SHA-512 SP 800-90A DRBG as the internal DRBG providing a seed generation.
+     * To configure this use “C:HYBRID;ENABLE{All};”
      */
     public Provider getProvider() {
-        return new BouncyCastleFipsProvider();
+        return new BouncyCastleFipsProvider("C:HYBRID;ENABLE{All};");
     }
 }
