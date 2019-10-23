576ead9a49c8630d5ab2a1d0edf78e084539e8a2
Dhruvil Shah <dhruvil@confluent.io>
2019-10-23 12:56:06 -0400

    CONFLUENT: Improve TierTopicInitLeader#toString formatting (#851)

--- core/src/main/java/kafka/tier/domain/TierTopicInitLeader.java
+++ core/src/main/java/kafka/tier/domain/TierTopicInitLeader.java
@@ -82,7 +82,7 @@ public class TierTopicInitLeader extends AbstractTierMetadata {
                 "version=" + version() + ", " +
                 "topicIdPartition=" + topicIdPartition() + ", " +
                 "tierEpoch=" + tierEpoch() + ", " +
-                "messageIdAsBase64=" + messageIdAsBase64() +
+                "messageIdAsBase64=" + messageIdAsBase64() + ", " +
                 "brokerId=" + brokerId() +
                 ")";
     }
