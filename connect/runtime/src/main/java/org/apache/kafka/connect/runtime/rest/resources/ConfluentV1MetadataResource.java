6855f89618bc61906dd49daeddfd492566f3838a
Chris Egerton <fearthecellos@gmail.com>
2019-11-13 12:36:09 -0800

    CONFLUENT: CC-6522: Use non-null default Kafka cluster ID if herder cannot deduce one (#924)
    * CC-6522: Use non-null default Kafka cluster ID if herder cannot deduce one
    
    * Update connect/runtime/src/main/java/org/apache/kafka/connect/runtime/rest/resources/ConfluentV1MetadataResource.java
    
    Co-Authored-By: Randall Hauch <rhauch@gmail.com>

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/rest/resources/ConfluentV1MetadataResource.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/rest/resources/ConfluentV1MetadataResource.java
@@ -6,6 +6,8 @@ import org.apache.kafka.connect.runtime.Herder;
 import org.apache.kafka.connect.runtime.WorkerConfig;
 import org.apache.kafka.connect.runtime.distributed.DistributedConfig;
 import org.apache.kafka.connect.runtime.rest.entities.ConfluentConnectClusterId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -16,10 +18,22 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 public class ConfluentV1MetadataResource {
 
+    public static final String UNKNOWN_KAFKA_CLUSTER_ID = "UNKNOWN";
+
+    private static final Logger log = LoggerFactory.getLogger(ConfluentV1MetadataResource.class);
+
     private final ConfluentConnectClusterId clusterId;
 
     public ConfluentV1MetadataResource(Herder herder, WorkerConfig workerConfig) {
         String kafkaClusterId = herder.kafkaClusterId();
+        if (kafkaClusterId == null) {
+            log.warn(
+                "Unable to fetch cluster ID from pre-0.10.1 Kafka broker, defaulting to '{}'",
+                UNKNOWN_KAFKA_CLUSTER_ID
+            );
+            kafkaClusterId = UNKNOWN_KAFKA_CLUSTER_ID;
+        }
+
         String connectClusterId = workerConfig.originals()
             .getOrDefault(DistributedConfig.GROUP_ID_CONFIG, "STANDALONE")
             .toString();
