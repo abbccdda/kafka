0df2cc66a649297e056531544556095b943c6a93
Lucas Bradstreet <lucas@confluent.io>
2020-01-06 15:41:30 -0800

    MINOR: fix toString typo in objectIdAsBase64 (#1122)

--- core/src/main/java/kafka/tier/domain/TierSegmentDeleteComplete.java
+++ core/src/main/java/kafka/tier/domain/TierSegmentDeleteComplete.java
@@ -80,6 +80,6 @@ public class TierSegmentDeleteComplete extends AbstractTierSegmentMetadata {
                 "version=" + metadata.version() + ", " +
                 "topicIdPartition=" + topicIdPartition() + ", " +
                 "tierEpoch=" + tierEpoch() + ", " +
-                "objectIdAsBAse64=" + objectIdAsBase64() + ")";
+                "objectIdAsBase64=" + objectIdAsBase64() + ")";
     }
 }
