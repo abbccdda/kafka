c0e9cc02677617763ad50df71e00cb3ff25a71f7
Brian Byrne <bbyrne@confluent.io>
2020-04-16 08:31:31 -0700

    KCORE-42 (1/-): Adds ClusterLinkScheduler for handling periodic tasks (#1494)
    Scheduler for performing periodic tasks for linked clusters.

---
+++ core/src/main/scala/kafka/server/link/ClusterLinkScheduler.scala
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ */
+
+package kafka.server.link
+
+import java.util.concurrent.TimeUnit
+
+import kafka.utils.{KafkaScheduler, Logging}
+import org.apache.kafka.common.KafkaFuture
+import org.apache.kafka.common.internals.KafkaFutureImpl
+
+class ClusterLinkScheduler extends KafkaScheduler(threads = 1, threadNamePrefix = "cluster-link-scheduler") {
+
+  /**
+    * Schedules the callback to be run when the future has completed in an asynchronous manner.
+    * The callback will be invoked from the scheduler's thread and should not block the thread.
+    *
+    * @param name name identifying the work
+    * @param future the future to wait for completion
+    * @param callback the callback to be invoked upon future completion
+    */
+  def scheduleWhenComplete[T](name: String, future: KafkaFuture[T], callback: () => Unit): Unit = {
+    future.whenComplete(new KafkaFuture.BiConsumer[T, Throwable] {
+      def accept(obj: T, throwable: Throwable): Unit = {
+        scheduleOnce(name, callback)
+      }
+    })
+  }
+}
+
+object ClusterLinkScheduler {
+  /**
+    * A task that's periodically performed on the cluster link's scheduler. The tasks may perform
+    * work that takes an indefinite amount of time to complete, and therefore an asynchronous
+    * context is required.
+    *
+    * A task's `run()` will be called from the scheduler when it's delay period has elapsed. From
+    * there, the task should start its work and submit any asynchronous callbacks through the
+    * `PeriodicTask` (e.g. via `scheduleWhenComplete()`), where the callback should return a boolean
+    * indicating whether the task has completed. If an exception is thrown, it's assumed that the task
+    * has completed in a failed way, and will be scheduled to run again after its delay period.
+    *
+    * @param scheduler the scheduler to run on
+    * @param name the name identifying the task's work
+    * @param periodMs the delay between subsequent tasks
+    */
+  abstract class PeriodicTask(val scheduler: ClusterLinkScheduler, val name: String, val periodMs: Int) extends Logging {
+
+    private var runOnceResult: Option[KafkaFutureImpl[Void]] = None
+    @volatile private var isShuttingDown = false
+
+    /**
+      * Begins the task's execution.
+      */
+    def startup(): Unit = {
+      scheduleRun()
+    }
+
+    /**
+      * Indicates that the task's execution should complete.
+      */
+    def shutdown(): Unit = {
+      isShuttingDown = true
+    }
+
+    /**
+      * Used for testing.
+      *
+      * Runs the task exactly once, returning a future which can be used to wait on the completion
+      * which will contain the task's exception, if thrown.
+      *
+      * The task must *not* have been started via `startup()`.
+      *
+      * @return a future for the task's result
+      */
+    def runOnce(): KafkaFuture[Void] = {
+      val result = new KafkaFutureImpl[Void]
+      runOnceResult = Some(result)
+      scheduleOnce(() => run())
+      result
+    }
+
+    /**
+      * Starts running the task, returning whether the task has completed.
+      *
+      * @return `true` if the task has completed, otherwise `false` if there's outstanding work to be done
+      */
+    protected def run(): Boolean
+
+    /**
+      * Schedules the callback to be run. The callback should return `true` if the task has completed,
+      * otherwise `false` if there's outstanding work to be done.
+      *
+      * @param callback the callback to be invoked
+      */
+    protected def scheduleOnce(callback: () => Boolean): Unit = {
+      scheduler.scheduleOnce(name, wrap(callback))
+    }
+
+    /**
+      * Schedules the callback to be run when the future has completed. The callback should return `true` if
+      * the task has completed, otherwise `false` if there's outstanding work to be done.
+      *
+      * @param future the future to wait for completion
+      * @param callback the callback to be invoked upon future completion
+      */
+    protected def scheduleWhenComplete[T](future: KafkaFuture[T], callback: () => Boolean): Unit = {
+      scheduler.scheduleWhenComplete(name, future, wrap(callback))
+    }
+
+    /**
+      * Invokes the callback. This should only be called from the scheduler's thread. If the callback
+      * indicates the task has completed, then the task is scheduled to be restarted after the
+      * delay period has elapsed.
+      *
+      * @param callback the callback to be invoked
+      */
+    private def invoke(callback: () => Boolean): Unit = {
+      if (!isShuttingDown) {
+        try {
+          if (callback()) {
+            runOnceResult match {
+              case Some(result) =>
+                result.complete(null)
+              case None =>
+                scheduleRun()
+            }
+          }
+        } catch {
+          case e: Throwable =>
+            warn(s"Encountered error while performing cluster link task '${name}': ${e}")
+            runOnceResult match {
+              case Some(result) =>
+                result.completeExceptionally(e)
+              case None =>
+                scheduleRun()
+            }
+        }
+      }
+    }
+
+    /**
+      * Wraps the provided callback to be called from `invoke()`.
+      *
+      * @param callback the callback to wrap
+      * @return the wrapped callback
+      */
+    private def wrap(callback: () => Boolean): () => Unit = () => invoke(callback)
+
+    /**
+      * Schedules the task to be restarted after the delay period.
+      */
+    private def scheduleRun() = if (!isShuttingDown) {
+      scheduler.schedule(name, wrap(() => run()), delay = periodMs, period = -1L, TimeUnit.MILLISECONDS)
+    }
+  }
+
+}
---
+++ core/src/test/scala/unit/kafka/server/link/ClusterLinkSchedulerTest.scala
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ */
+
+package kafka.server.link
+
+import java.util.concurrent.{Exchanger, ExecutionException, TimeUnit}
+
+import org.apache.kafka.common.errors.InvalidRequestException
+import org.apache.kafka.common.internals.KafkaFutureImpl
+import org.junit.{After, Before, Test}
+import org.junit.Assert._
+
+class ClusterLinkSchedulerTest {
+
+  val scheduler = new ClusterLinkScheduler()
+  var task: ClusterLinkScheduler.PeriodicTask = null
+
+  private val exchanger = new Exchanger[Integer]()
+  private def exchangeExpect(value: Integer, expect: Integer): Unit =
+    assertEquals(expect, exchanger.exchange(value, 1, TimeUnit.SECONDS))
+  private def testNotify(value: Integer): Unit = exchangeExpect(value, null)
+  private def testWait(value: Integer): Unit = exchangeExpect(null, value)
+
+  @Before
+  def setUp(): Unit = {
+    scheduler.startup()
+  }
+
+  @After
+  def tearDown(): Unit = {
+    if (task != null)
+      task.shutdown()
+    scheduler.shutdown()
+  }
+
+  @Test
+  def testScheduleWhenComplete(): Unit = {
+    val future1 = new KafkaFutureImpl[Void]()
+    scheduler.scheduleWhenComplete("test", future1, () => testNotify(1))
+    Thread.sleep(10)
+    future1.complete(null)
+    testWait(1)
+
+    val future2 = new KafkaFutureImpl[Void]()
+    scheduler.scheduleWhenComplete("test", future2, () => testNotify(2))
+    Thread.sleep(10)
+    future2.completeExceptionally(new InvalidRequestException(""))
+    testWait(2)
+  }
+
+  @Test
+  def testPeriodicTaskPeriod(): Unit = {
+    val taskPeriodMs = 10
+
+    task = new ClusterLinkScheduler.PeriodicTask(scheduler, name = "TestTask", periodMs = taskPeriodMs) {
+      private var first = true
+
+      override def run(): Boolean = {
+        if (first) {
+          first = false
+          testNotify(1)
+        } else {
+          testNotify(2)
+        }
+        true
+      }
+    }
+
+    val start = System.nanoTime()
+    task.startup()
+    testWait(1)
+    testWait(2)
+    val diffMs = (System.nanoTime() - start) / 1e6
+    assertTrue(diffMs >= taskPeriodMs * 2)
+  }
+
+  @Test
+  def testPeriodicTaskSchedule(): Unit = {
+    val taskPeriodMs = 10
+
+    val future = new KafkaFutureImpl[Void]()
+
+    task = new ClusterLinkScheduler.PeriodicTask(scheduler, name = "TestTask", periodMs = taskPeriodMs) {
+      private var running = false
+      private var done = false
+
+      override def run(): Boolean = {
+        assertFalse(running)
+
+        if (done) {
+          testNotify(6)
+          true
+        } else {
+          running = true
+
+          testNotify(1)
+          testWait(2)
+          scheduleOnce(() => next())
+          false
+        }
+      }
+
+      def next(): Boolean = {
+        assertTrue(running)
+        assertFalse(done)
+
+        testNotify(3)
+        testWait(4)
+        scheduleWhenComplete(future, () => finish())
+        false
+      }
+
+      def finish(): Boolean = {
+        assertTrue(running)
+        assertFalse(done)
+
+        testNotify(5)
+        running = false
+        done = true
+        true
+      }
+
+    }
+
+    task.startup()
+    testWait(1)
+    testNotify(2)
+    testWait(3)
+    testNotify(4)
+    Thread.sleep(taskPeriodMs)
+    future.complete(null)
+    testWait(5)
+    testWait(6)
+  }
+
+  @Test
+  def testPeriodicTaskException(): Unit = {
+    task = new ClusterLinkScheduler.PeriodicTask(scheduler, name = "TestTask", periodMs = 10) {
+      private var first = true
+
+      override def run(): Boolean = {
+        if (first) {
+          first = false
+          testNotify(1)
+          throw new InvalidRequestException("")
+        } else {
+          testNotify(2)
+          true
+        }
+      }
+
+    }
+
+    task.startup()
+    testWait(1)
+    testWait(2)
+  }
+
+  @Test
+  def testRunOnce(): Unit = {
+    task = new ClusterLinkScheduler.PeriodicTask(scheduler, name = "TestTask", periodMs = 10) {
+      private var first = true
+
+      override def run(): Boolean = {
+        if (first) {
+          first = false
+          testNotify(1)
+          true
+        } else {
+          throw new InvalidRequestException("")
+        }
+      }
+    }
+
+    val result = task.runOnce()
+    testWait(1)
+    result.get
+  }
+
+  @Test(expected = classOf[InvalidRequestException])
+  def testRunOnceException(): Unit = {
+    task = new ClusterLinkScheduler.PeriodicTask(scheduler, name = "TestTask", periodMs = 10) {
+      override def run(): Boolean = {
+        throw new InvalidRequestException("")
+      }
+    }
+
+    try {
+      task.runOnce().get
+    } catch {
+      case e: ExecutionException => throw e.getCause
+    }
+  }
+}
