a821c21bc82108c331a4b6d291f3bb9871312c29
Brian Byrne <bbyrne@confluent.io>
2020-04-16 08:51:27 -0700

    KCORE-42 (3/-): Implements merging of source + mirror topics' configs (#1497)

---
+++ core/src/main/scala/kafka/server/link/ClusterLinkUtils.scala
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ */
+package kafka.server.link
+
+import java.util.Properties
+
+import kafka.log.LogConfig
+import kafka.utils.Logging
+import org.apache.kafka.clients.admin.Config
+import org.apache.kafka.common.errors.InvalidConfigurationException
+
+import scala.collection.JavaConverters._
+
+object ClusterLinkUtils extends Logging {
+
+  private object LogConfigAction extends Enumeration {
+    type LogConfigAction = Value
+
+    // Independent: Never mirror the source's value. Instead, allow the value to be modified on the source.
+    // NonDefault: Only mirror if it's not a default value on the source, otherwise use the broker's local default.
+    //             This makes the config immutable on the destination.
+    // Always: Always mirror. This makes the config immutable on the destination.
+    val Independent, NonDefault, Always = Value
+  }
+
+  private val independentConfigs = List(
+    // Cluster-centric
+    LogConfig.TopicPlacementConstraintsProp,
+    LogConfig.UncleanLeaderElectionEnableProp,
+    LogConfig.MessageDownConversionEnableProp,
+    LogConfig.AppendRecordInterceptorClassesProp,
+
+    // Schema
+    LogConfig.KeySchemaValidationEnableProp,
+    LogConfig.ValueSchemaValidationEnableProp,
+    LogConfig.KeySchemaValidationStrategyProp,
+    LogConfig.ValueSchemaValidationStrategyProp,
+
+    // Tiering
+    LogConfig.TierEnableProp,
+    LogConfig.TierLocalHotsetBytesProp,
+    LogConfig.TierLocalHotsetMsProp,
+    LogConfig.TierSegmentHotsetRollMinBytesProp,
+    LogConfig.PreferTierFetchMsProp,
+
+    // Rebalance throttling
+    LogConfig.LeaderReplicationThrottledReplicasProp,
+    LogConfig.FollowerReplicationThrottledReplicasProp
+  )
+
+  private val nonDefaultConfigs = List(
+    LogConfig.SegmentBytesProp,
+    LogConfig.SegmentMsProp,
+    LogConfig.SegmentJitterMsProp,
+    LogConfig.SegmentIndexBytesProp,
+    LogConfig.FlushMessagesProp,
+    LogConfig.FlushMsProp,
+    LogConfig.RetentionBytesProp,
+    LogConfig.RetentionMsProp,
+    LogConfig.IndexIntervalBytesProp,
+    LogConfig.DeleteRetentionMsProp,
+    LogConfig.MinCompactionLagMsProp,
+    LogConfig.MaxCompactionLagMsProp,
+    LogConfig.FileDeleteDelayMsProp,
+    LogConfig.MinCleanableDirtyRatioProp,
+    LogConfig.MinInSyncReplicasProp,
+    LogConfig.PreAllocateEnableProp,
+    LogConfig.MessageFormatVersionProp,
+    LogConfig.MessageTimestampTypeProp,
+    LogConfig.MessageTimestampDifferenceMaxMsProp,
+    LogConfig.SegmentSpeculativePrefetchEnableProp
+  )
+
+  private val alwaysConfigs = List(
+    LogConfig.CleanupPolicyProp,
+    LogConfig.MaxMessageBytesProp,
+    LogConfig.CompressionTypeProp
+  )
+
+  private val configAction =
+    (independentConfigs.map(_ -> LogConfigAction.Independent) ++
+      nonDefaultConfigs.map(_ -> LogConfigAction.NonDefault) ++
+      alwaysConfigs.map(_ -> LogConfigAction.Always)).toMap
+
+  private def getConfigAction(name: String): LogConfigAction.Value = {
+    configAction.get(name) match {
+      case Some(action) => action
+      case None =>
+        warn(s"Unhandled configuration key '$name'")
+        LogConfigAction.Always
+    }
+  }
+
+  /**
+    * Initializes the log config properties for a mirrored topic. If the local properties contains
+    * any configurations that aren't permissible to be set by a mirrored topic, then an exception is
+    * thrown.
+    *
+    * @param topic the topic's name
+    * @param localProps the initial properties of the topic
+    * @param remoteConfig the log config of the source topic
+    * @return the initial log config properties
+    * @throws InvalidConfigurationException if the local properties set an immutable config
+    */
+  def initMirrorProps(topic: String, localProps: Properties, remoteConfig: Config): Properties =
+    resolveMirrorProps(localProps, remoteConfig, (name: String) =>
+      throw new InvalidConfigurationException(s"Cannot set configuration '$name' for mirror topic '$topic'")
+    )
+
+  /**
+    * Updates the log config properties for a mirrored topic to reflect changes of the source. Any configs
+    * set in the local properties that are mirrored (immutable) are assumed to be due to mirroring, and
+    * therefore it's not considered an error if they exist.
+    *
+    * @param topic the topic's name
+    * @param localProps the current properties of the topic
+    * @param remoteConfig the log config of the source topic
+    * @return the updated log config properties
+    */
+  def updateMirrorProps(topic: String, localProps: Properties, remoteConfig: Config): Properties =
+    resolveMirrorProps(localProps, remoteConfig, (name: String) =>
+      warn(s"Unexpected configuration '$name' set for mirror topic '$topic'")
+    )
+
+  /**
+    * Resolves the log config properties for a mirrored topic.
+    *
+    * @param localProps the current properties of the topic
+    * @param remoteConfig the log config of the source topic
+    * @param onInvalidConfig the action to take when an invalid config is detected
+    * @return the resolve log config properties
+    */
+  private def resolveMirrorProps(localProps: Properties, remoteConfig: Config, onInvalidConfig: String => Unit): Properties = {
+    val newLocalProps = new Properties()
+    val remoteEntries = remoteConfig.entries.asScala.map(e => e.name -> e).toMap
+
+    LogConfig.configNames.foreach { name =>
+      getConfigAction(name) match {
+        case LogConfigAction.Independent =>
+          val value = localProps.get(name)
+          if (value != null)
+            newLocalProps.put(name, value)
+
+        case LogConfigAction.NonDefault =>
+          if (localProps.containsKey(name))
+            onInvalidConfig(name)
+          remoteEntries.get(name) match {
+            case Some(remoteEntry) =>
+              if (!remoteEntry.isDefault)
+                newLocalProps.put(name, remoteEntry.value)
+            case None =>
+          }
+
+        case LogConfigAction.Always =>
+          if (localProps.containsKey(name))
+            onInvalidConfig(name)
+          remoteEntries.get(name).foreach(e => newLocalProps.put(e.name, e.value))
+      }
+    }
+
+    newLocalProps
+  }
+
+}
---
+++ core/src/test/scala/unit/kafka/server/link/ClusterLinkUtilsTest.scala
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2020 Confluent Inc.
+ */
+package kafka.server.link
+
+import java.util.Properties
+
+import kafka.log.LogConfig
+import org.apache.kafka.clients.admin.{Config, ConfigEntry}
+import org.apache.kafka.common.errors.InvalidConfigurationException
+import org.junit.Test
+import org.junit.Assert._
+
+import scala.collection.JavaConverters._
+
+class ClusterLinkUtilsTest {
+
+  @Test
+  def testInitMirrorProps(): Unit = {
+    val remoteConfig = makeConfig(Seq(
+      (LogConfig.CleanupPolicyProp, "compact", false),
+      (LogConfig.MaxMessageBytesProp, "10485760", true),
+      (LogConfig.UncleanLeaderElectionEnableProp, "true", false),
+      (LogConfig.PreferTierFetchMsProp, "100000", true),
+      (LogConfig.SegmentMsProp, "100000", false),
+      (LogConfig.SegmentBytesProp, "1048576", true)
+    ))
+
+    val localProps = new Properties()
+    localProps.put(LogConfig.MessageDownConversionEnableProp, "true")
+
+    val newLocalProps = ClusterLinkUtils.initMirrorProps("test", localProps, remoteConfig)
+
+    val expectedLocalProps = new Properties()
+    expectedLocalProps.put(LogConfig.MessageDownConversionEnableProp, "true")
+    expectedLocalProps.put(LogConfig.CleanupPolicyProp, "compact")
+    expectedLocalProps.put(LogConfig.MaxMessageBytesProp, "10485760")
+    expectedLocalProps.put(LogConfig.SegmentMsProp, "100000")
+    assertEquals(expectedLocalProps, newLocalProps)
+  }
+
+  @Test(expected = classOf[InvalidConfigurationException])
+  def testInitMirrorPropsLocalNonDefault(): Unit = {
+    val localProps = new Properties()
+    localProps.put(LogConfig.SegmentMsProp, "100000")
+    ClusterLinkUtils.initMirrorProps("test", localProps, makeConfig(Seq.empty))
+  }
+
+  @Test(expected = classOf[InvalidConfigurationException])
+  def testInitMirrorPropsLocalAlways(): Unit = {
+    val localProps = new Properties()
+    localProps.put(LogConfig.CleanupPolicyProp, "compact")
+    ClusterLinkUtils.initMirrorProps("test", localProps, makeConfig(Seq.empty))
+  }
+
+  @Test
+  def testInitMirrorPropsUnknownConfigIgnored(): Unit = {
+    val remoteConfig = makeConfig(Seq(("bad.config.key", "12345", false)))
+    val localProps = new Properties()
+    val newLocalProps = ClusterLinkUtils.initMirrorProps("test", localProps, remoteConfig)
+    assertTrue(newLocalProps.isEmpty)
+  }
+
+  @Test
+  def testUpdateMirrorProps(): Unit = {
+    val remoteConfig = makeConfig(Seq(
+      (LogConfig.CleanupPolicyProp, "compact", false),
+      (LogConfig.MaxMessageBytesProp, "10485760", true),
+      (LogConfig.UncleanLeaderElectionEnableProp, "true", false),
+      (LogConfig.PreferTierFetchMsProp, "100000", true),
+      (LogConfig.SegmentMsProp, "100000", false),
+      (LogConfig.SegmentBytesProp, "1048576", true)
+    ))
+
+    val localProps = makeProperties(Seq(
+      LogConfig.CleanupPolicyProp -> "delete",
+      LogConfig.MaxMessageBytesProp -> "524288",
+      LogConfig.MessageDownConversionEnableProp -> "true",
+      LogConfig.UncleanLeaderElectionEnableProp -> "false",
+      LogConfig.SegmentMsProp -> "200000",
+      LogConfig.SegmentBytesProp -> "1048576",
+    ))
+
+    val expectedLocalProps = makeProperties(Seq(
+      LogConfig.CleanupPolicyProp -> "compact",
+      LogConfig.MaxMessageBytesProp -> "10485760",
+      LogConfig.MessageDownConversionEnableProp -> "true",
+      LogConfig.UncleanLeaderElectionEnableProp -> "false",
+      LogConfig.SegmentMsProp -> "100000",
+    ))
+
+    val newLocalProps = ClusterLinkUtils.updateMirrorProps("test", localProps, remoteConfig)
+    assertEquals(expectedLocalProps, newLocalProps)
+  }
+
+  @Test
+  def testUpdateMirrorPropsUnknownConfigIgnored(): Unit = {
+    val remoteConfig = makeConfig(Seq(("bad.config.key", "12345", false)))
+    val localProps = makeProperties(Seq("another.bad.config.key" -> "23456"))
+    val newLocalProps = ClusterLinkUtils.updateMirrorProps("test", localProps, remoteConfig)
+    assertTrue(newLocalProps.isEmpty)
+  }
+
+  private def makeConfig(entries: Seq[(String, String, Boolean)]): Config = {
+    val configEntries = entries.map { entry =>
+      val source = if (entry._3)
+        ConfigEntry.ConfigSource.DEFAULT_CONFIG
+      else
+        ConfigEntry.ConfigSource.DYNAMIC_TOPIC_CONFIG
+      new ConfigEntry(entry._1, entry._2, source, false, false, List.empty[ConfigEntry.ConfigSynonym].asJava)
+    }
+    new Config(configEntries.asJavaCollection)
+  }
+
+  private def makeProperties(entries: Seq[(String, String)]): Properties = {
+    val props = new Properties()
+    entries.foreach { case (name, value) =>
+      props.put(name, value)
+    }
+    props
+  }
+}
