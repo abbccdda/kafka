322ebd78f9bc654b45285e9b6fb7927b4798f2a2
Brian Byrne <bbyrne@confluent.io>
2020-06-09 08:45:10 -0700

    KC-582: Enable multi-tenant support for alterMirrors API. (#1904)

--- ce-broker-plugins/src/main/java/io/confluent/kafka/multitenant/schema/MultiTenantApis.java
+++ ce-broker-plugins/src/main/java/io/confluent/kafka/multitenant/schema/MultiTenantApis.java
@@ -101,6 +101,7 @@ public class MultiTenantApis {
       case CREATE_CLUSTER_LINKS:
       case LIST_CLUSTER_LINKS:
       case DELETE_CLUSTER_LINKS:
+      case ALTER_MIRRORS:
         return true;
 
       case CONTROLLED_SHUTDOWN:
@@ -121,7 +122,6 @@ public class MultiTenantApis {
       case ALTER_CLIENT_QUOTAS:
       case REMOVE_BROKERS:
       case INITIATE_SHUTDOWN:
-      case ALTER_MIRRORS:
       case DESCRIBE_BROKER_REMOVALS:
         return false;
 
